e7a63dbf89762b86f34b4eca9dd1b7755371ce21
MALKIELNILI <malkielnili@gmail.com>
2026-06-11 14:27:50 +0300

    Allow entering past charges with custom date for Shmulik and Engel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'melavim-v20260611-1413';
+const CACHE = 'melavim-v20260611-1505';
 const FILES = ['./','./index.html','./manifest.json','./icon.jpg'];
 
 self.addEventListener('install', e => {
